5bb8275d5935115c5c60ddda6aca2100f542655c
steve omari <steveomari62@gmail.com>
2023-03-09 20:38:30 -0500

    Gif-maker example/template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
 import "./style.css";
+
+
+const img = document.querySelector("img");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+// const searchTerm = prompt("What Gif do you want to look up next?");
+let searchTerm = "cats";
+
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    button.click();
+  }
+});
+
+button.addEventListener("click", () => {
+  if (input.value === "" || input.value === undefined) {
+    return;
+  } else {
+    searchTerm = input.value;
+
+    fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=SNx0tORQLx6LOnuvvFOR1GR6g5GIa6gc&s=${searchTerm}`,
+      { mode: "cors" }
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        img.src = response.data.images.original.url;
+      })
+      .catch(function (error) {
+        console.log("Unable to find, please try again.");
+        return;
+      });
+  }
+});
+
+fetch(
+  `https://api.giphy.com/v1/gifs/translate?api_key=SNx0tORQLx6LOnuvvFOR1GR6g5GIa6gc&s=${searchTerm}`,
+  { mode: "cors" }
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (response) {
+    img.src = response.data.images.original.url;
+  });
